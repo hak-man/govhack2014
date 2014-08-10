06c0369d6bb9dd2c0b32e5fb90faa90293f77d45
Greg Bowering <gerg.bowering@gmail.com>
2014-08-10 16:09:20 +0930

    More updates to Google Earth Public Transport Visualisation, but run out of time for MOD deadline :/

--- transit-vis/KmlUpdate.js
+++ transit-vis/KmlUpdate.js
@@ -16,11 +16,14 @@ var KML_PORT = 1338;
 
 // var VEHICLE_ICON_URL = 'web/path/to/PNG/icon/to/display/in/sidebar/tree';
 
-var VEHICLE_DAE_LINK = "<Link><href>file://ewrd-smb/data/slew_uav/Aircraft%20Pictures/models/AerosondeUAV1.dae</href></Link>\n";
+var VEHICLE_DAE_LINK = "<Link><href>http://users.on.net/~gb1/dae/Adelaide%20Alstom%20Citadis%20302.dae</href></Link>\n";
 
-var VEHICLE_DAE_SCALE = 0.1;
+var VEHICLE_DAE_SCALE = 1.0;
 
 var VEHICLE_SCALE_KML = kml.scale3(VEHICLE_DAE_SCALE, VEHICLE_DAE_SCALE, VEHICLE_DAE_SCALE);
+
+// model is not always oriented North-South (0 degrees) so need to apply an offset when rendering:
+var VEHICLE_ORIENTATION_OFFSET = 90.0;
 
 // END CONFIGURATION SECTION
 
@@ -35,65 +38,80 @@ var http = require('http'); // for HTTP server to serve KML position updates to 
 
 // previous positions required to determine heading
 var prevPositions = {};
-var currPositions = {};
+var currPositions = {"TramId": new kml.Coords3("TramId", 138.587222, -34.922150)};
 var currHeadings = {};
 var prevHeadings = {};
 
+// Timer Callback to update all vehicle positions each second:
+var updateEverySecond = function() {
+  // TODO use SIRI realtime service to determine accurate vehicle position.
+  // FIXME for now just using dummy position updates!
+
+  // Dummy movement vectors to move a vehcile.
+  var dummyMoveNorth = new kml.Coords3("moveNorth",  0.0,  0.00001);
+  var dummyMoveSouth = new kml.Coords3("moveSouth",  0.0, -0.00001);
+  var dummyMoveEast = new kml.Coords3("moveEast",  0.00001, 0);
+  var dummyMoveWest = new kml.Coords3("moveWest", -0.00001, 0);
+
+  for (service_id in currPositions) {
+//    console.log("updating service_id " + service_id);
+
+    var currPosition = currPositions[service_id];
+    prevPositions[service_id] = currPosition;
+    currPosition = currPosition.add(dummyMoveEast);
+    currPositions[service_id] = currPosition;
+  }
+}
+setInterval(updateEverySecond, 1000);
+
+
+
+// This Web Server handles client network link update requests.
+// FIXME should have no vehicle postion/orientation processing here.
+// All vehicle processing should be moved to the updateEverySecond callback.
+// This server should only read current vehicle positions and respond with KML to clients.
 var s = http.createServer(function httpServe(req, res) {
   
   var expiryDate = new Date();
-  expiryDate.setTime(expiryDate.getTime() + 6 * 1000); // expire in 6 seconds from now
+  expiryDate.setTime(expiryDate.getTime() + 30 * 1000); // expire in 30 seconds from now
   var expiryString = kml.ISODateString(expiryDate);
   
   // var time = expiryDate.getTime();
   
   var kmlStr = ''
     + kml.name('Transport Vehicle Position Update')
-    + kml.atomAuthorLink('Greg Bowering', 'mailto:gerg.bowering@gmail.com')
-    + kml.style('yellowLineGreenPoly', ''
-      + kml.lineStyleColorWidth('7f00ffff', 4)
-      + kml.polyStyle('7f00ff00')
-      );
+    + kml.atomAuthorLink('Greg Bowering', 'mailto:gerg.bowering@gmail.com');
       
   for (service_id in prevPositions) {
     // Plot positions including heading and movement vectors for each service_id
+    console.log("plotting service_id " + service_id);
+    
     var prevPosition = prevPositions[service_id];
     var currPosition = currPositions[service_id];
     var headingDegrees;
     
     if (service_id in currHeadings && currHeadings[service_id] != 0.0) {
       headingDegrees = currHeadings[service_id];
+      prevHeadings[service_id] = headingDegrees;
     } else {
       // The following calculates heading based on change in position:
       if (prevPosition === undefined) {
       	headingDegrees = 0;
-      	prevPosition = [];
-      	prevPositions[service_id] = prevPosition;
-      } else if (prevPosition.length == 0) {
-      	headingDegrees = 0;
       } else {
-        var positionChange = currPosition.subtract(prevPosition[0]);
+        var positionChange = currPosition.subtract(prevPosition);
         headingDegrees = positionChange.headingDegrees();
+        console.log('positionChange=' + positionChange.toKml() + ' quadrant=' + positionChange.quadrant() + ' heading=' + headingDegrees);
       }
     }
-    
-    
+        
     var deltaAlt;
-    if (prevPosition === undefined || prevPosition.length == 0) {
+    if (prevPosition === undefined) {
       deltaAlt = 0;
     } else {
-      deltaAlt = currPosition.alt - prevPosition[0].alt;
+      deltaAlt = currPosition.alt - prevPosition.alt;
     }
     
-      // console.log('positionChange=' + positionChange.toKml() + ' quadrant=' + positionChange.quadrant() + ' heading=' + headingDegrees);
            
-      var prevPositionKml = '';
-      for (i in prevPosition) {
-        if (prevPosition[i] != undefined) {
-          prevPositionKml += prevPosition[i].toKml() + '\n';
-        }
-      }
-      
       var tiltDegrees = 0.0;
       // fudge tilt based on change in altitude (vehicle going up or down hill)
       tiltDegrees = -2.0*deltaAlt;
@@ -104,37 +122,23 @@ var s = http.createServer(function httpServe(req, res) {
       // assume transit vehicles never "roll"
       var rollDegrees = 0.0;
 
-      prevHeadings[service_id] = headingDegrees;
-      
-      var orientation = new kml.Orientation(service_id, headingDegrees, tiltDegrees, rollDegrees);
+//      currHeadings[service_id] = headingDegrees; // no! do not persist derived heading here
+    
+      var orientation = new kml.Orientation(service_id, headingDegrees + VEHICLE_ORIENTATION_OFFSET, tiltDegrees, rollDegrees);
       
 	kmlStr += ''
 	  + kml.placemark(''
-	    + kml.name('UAV-' + service_id)
+	    + kml.name('Vehicle-' + service_id)
 	    + kml.model('model_' + service_id, ''
-	      + kml.altitudeMode()
+	      + kml.altitudeMode('clampToGround') // Default altitudeMode is 'absolute', better to clamp surface-vehicles.
 	      + currPosition.toLocationKml() + '\n' // Location
 	      + orientation.toKml() + '\n' // Orientation
 	      + VEHICLE_SCALE_KML
 	      + VEHICLE_DAE_LINK // Model Link (and optional ResourceMap for textures)
 	      )
-	    )
-	  + kml.placemark(''
-	    + kml.name('track-' + service_id)
-	    + kml.description('Transparent green wall with yellow outlines')
-	    + kml.styleUrl('#yellowLineGreenPoly')
-	    + kml.lineString(''
-	      + kml.extrude()
-	      + kml.tessellate()
-	      + kml.altitudeMode()
-	      + kml.coordinates(''
-		+ currPosition.toKml() + '\n' // New Location
-		+ prevPositionKml + '\n' // Old Locations
-	        )
-	      )
 	    );
-     
-  }
+	    
+  } // for (service_id in prevPositions)
    
    kmlStr = kml.kml(
      kml.networkLinkControl(kml.expires(expiryString))
@@ -160,7 +164,5 @@ function degreesMinutesToDecimalDegrees(degreesMinutes, pole) {
 	return decimalDegrees;
 }
 
-// TODO
-// Every second we query SIRI web service for current position of vehicle (Requires SIRI Vehicle Monitoring service)
-// Update postions array with current reported position of vehicle
+// FIXME this file needs reformatting.  Need to edit in a javascript IDE rather than text editor!
 
